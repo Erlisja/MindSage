b816b388e47b40730c2b8e1abb591c2dba505997
Abishek Jayan <abishekjayan98@gmail.com>
2024-07-13 15:09:58 +0530

    Completed basic layout of chat page

--- app/src/main/java/com/example/mindsage/FirstPage.kt
+++ app/src/main/java/com/example/mindsage/FirstPage.kt
@@ -2,12 +2,23 @@ package com.example.mindsage
 
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class FirstPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.startButton)
+
+        button.setOnClickListener {
+            val intent = Intent(this, ChatPage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
