f2006060c02040042a263f5d154577c48ae8681e
Erlisja <133474314+Erlisja@users.noreply.github.com>
2024-07-12 22:45:37 -0400

    Main page layout

--- app/src/main/java/com/example/mindsage/FirstPage.kt
+++ app/src/main/java/com/example/mindsage/FirstPage.kt
@@ -1,16 +1,13 @@
 package com.example.mindsage
 
-import android.content.Intent
+
+
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mindsage.R
 
 class FirstPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
